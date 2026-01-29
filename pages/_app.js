d0d10702ee0557e48998b80193c9872d1f54ae9e
shareef6907 <shareef6907@users.noreply.github.com>
2026-01-29 13:29:42 +0300

    Add favicon from bahrainnights.com

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,10 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+        <link rel="icon" href="/favicon-48x48.png" type="image/png" sizes="48x48" />
       </Head>
       {getLayout(<Component {...pageProps} />)}
     </>
